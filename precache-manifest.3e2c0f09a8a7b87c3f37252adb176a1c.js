2fb648187e7324f8b4fe5536b125cfd4ee57b629
sagar-prasain <53586253+sagar-prasain@users.noreply.github.com>
2021-12-21 20:05:35 +1100

    Updates

---
+++ precache-manifest.3e2c0f09a8a7b87c3f37252adb176a1c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7736a40dde53ee30a809d06e53a52897",
+    "url": "/react-website-store/index.html"
+  },
+  {
+    "revision": "d2fa625a19bfb264adab",
+    "url": "/react-website-store/static/css/main.40d73413.chunk.css"
+  },
+  {
+    "revision": "7c3d6c8e5e7e3cbab735",
+    "url": "/react-website-store/static/js/2.1e53a480.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/react-website-store/static/js/2.1e53a480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5682aaa46ed221e048",
+    "url": "/react-website-store/static/js/3.b824625f.chunk.js"
+  },
+  {
+    "revision": "d2fa625a19bfb264adab",
+    "url": "/react-website-store/static/js/main.6108eb78.chunk.js"
+  },
+  {
+    "revision": "566ec5b358756d4bb4c9",
+    "url": "/react-website-store/static/js/runtime-main.dc488d2c.js"
+  },
+  {
+    "revision": "4871ba71cfc56052455ef4b835fc108d",
+    "url": "/react-website-store/static/media/Bolog.4871ba71.jpg"
+  },
+  {
+    "revision": "da7fc72267ad7c43b2ef8cc574e08cc2",
+    "url": "/react-website-store/static/media/Fruit.da7fc722.jpg"
+  },
+  {
+    "revision": "5b635e781e7baad892944dd48bc21960",
+    "url": "/react-website-store/static/media/MS.5b635e78.jpg"
+  },
+  {
+    "revision": "ab8c2470ee477f1a7b18f6936bffd958",
+    "url": "/react-website-store/static/media/OJ.ab8c2470.jpg"
+  },
+  {
+    "revision": "9352ee5ce0101d5959a107d680de8dab",
+    "url": "/react-website-store/static/media/Trout.9352ee5c.jpg"
+  },
+  {
+    "revision": "096f59eb5c414d7553055906f8fd12d1",
+    "url": "/react-website-store/static/media/b.096f59eb.jpg"
+  },
+  {
+    "revision": "e29e930c1870f3fcc15edb3a8c3ade19",
+    "url": "/react-website-store/static/media/pizzaLeft.e29e930c.jpg"
+  },
+  {
+    "revision": "30cec7e260c2239513902ec3bcf69f59",
+    "url": "/react-website-store/static/media/whippy.30cec7e2.jpg"
+  }
+]);
